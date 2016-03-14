0c23bb28738007df7eaed4faad7943788ea75d1b
Dmitry Litvinenko <anti1869@gmail.com>
2016-03-14 11:25:37 +0300

    Testing airline destination settings

--- src/aeroport/airlines/like/common.py
+++ src/aeroport/airlines/like/common.py
@@ -2,18 +2,20 @@
 Common utilities for this airline.
 """
 
+import os
+
 STREAM_CONFIG = {
     "streams": {
         "rabbitmq": {
             "transport": "sunhead.events.transports.amqp.AMQPClient",
             "connection_parameters": {
-                "login": "guest",
-                "password": "",
-                "host": "localhost",
-                "port": 5672,
-                "virtualhost": "/",
+                "login": os.environ.get("AIR_LIKE_STREAM_USER", "guest"),
+                "password": os.environ.get("AIR_LIKE_STREAM_PASSWORD", ""),
+                "host": os.environ.get("AIR_LIKE_STREAM_HOST", "localhost"),
+                "port": int(os.environ.get("AIR_LIKE_STREAM_PORT", 5672)),
+                "virtualhost":  os.environ.get("AIR_LIKE_STREAM_VIRTUALHOST", "/"),
             },
-            "exchange_name": "like_bus",
+            "exchange_name":  os.environ.get("AIR_LIKE_STREAM_EXCHANGE", "like_bus"),
             "exchange_type": "topic",
         },
     },
